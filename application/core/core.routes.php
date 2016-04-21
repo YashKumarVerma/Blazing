5d504bf65ec04faaaff35c4a62a3b82073ec6c32
YashKumarVerma <yk.verma2000@gmail.com>
2016-04-21 12:44:40 +0530

    cleaned core.routes.php

--- application/core/core.routes.php
+++ application/core/core.routes.php
@@ -8,61 +8,6 @@ class Route
 	| 	homepage@main_form where
 	|	@main_form : is the method
 	***********************************************/
-/*
-	static function set($string, $controller_method)
-	{
-		$request = io::url()[0];
-		if($request == $string)
-		{
-			$controller = controller::extract($controller_method)[0];
-			$function 	= controller::extract($controller_method)[1];
-			if(controller::exist($controller))
-			{
-				include_once "application/modules/controller/".$controller.".controller.php";
-				if(function_exists($function))
-					{
-						call_user_func($function);
-					}
-				else
-					{
-						error::fatal('Route : 2','undefined function called !');
-					}
-				
-			}
-			else
-			{
-				error::fatal('Route : 1','undefined controller called');
-			}
-		}
-	}
-
-	static function post($string,$controller_method)
-	{
-		$request = io::url()[0];
-		if($request == $string && count($_POST) != 0 )
-		{
-
-			$controller = controller::extract($controller_method)[0];
-			$function 	= controller::extract($controller_method)[1];
-			if(controller::exist($controller))
-			{
-				include_once "application/modules/controller/".$controller.".controller.php";
-				if(function_exists($function))
-				{
-					call_user_func($function);
-				}
-				else
-				{
-					error::fatal('Route : 2' , 'undefined function called');
-				}
-			}
-			else
-			{
-				error::fatal('Route : 1','undefined controller called');
-			}
-		}
-	}
-*/
 static function set($string , $controller_method)
 	{
 		$broken_url = explode('/', io::get('url'));
