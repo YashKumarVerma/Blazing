3a4312857a0f0e9a37639dfc50e755dfa57dc4c0
YashKumarVerma <yk.verma2000@gmail.com>
2016-04-22 12:39:00 +0530

    added file->create()

--- application/modules/model/handler_file.model.php
+++ application/modules/model/handler_file.model.php
@@ -34,8 +34,32 @@ class file_helper_class
 	// to read all data
 	public function read()
 	{
-		return file_get_contents($this->file_location);
+		if(file_exists($this->file_location))
+		{
+			return file_get_contents($this->file_location);
+		}
+		else
+		{
+			return "File Not Found";
+		}
 	}
+
+	// to create file
+	public function create()
+	{
+		if(file_exists($this->file_location))
+		{
+			// return false is file already exists
+			return FALSE;
+		}
+		else
+		{
+			// file created
+			return TRUE;
+		}
+	}
+
+
 }
 
 
@@ -48,3 +72,5 @@ class file_helper_class
 
 * Load the contents of target file by
 @ $handler->file('filename')->read();
+
+*/
