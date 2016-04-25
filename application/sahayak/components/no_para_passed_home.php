04027832c4602908f68502b5390af1339f1a76b5
YashKumarVerma <yk.verma2000@gmail.com>
2016-04-25 15:07:55 +0530

    added controller syntax

--- application/sahayak/components/no_para_passed_home.php
+++ application/sahayak/components/no_para_passed_home.php
@@ -6,3 +6,16 @@ ___________________________________________________
   Sahayak : Command Line Utility for Blazing
 
 ---------------------------------------------------
+
+Basic Syntax :-
+$ php sahayak <domain> <sub-domain> <arguments>
+
+--------------------------------------------------- 
+
+Domains :-
+	controller
+	blazer
+
+Subdomains :-
+	Controller :-
+		create
