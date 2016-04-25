9a12d08f25e7a9a2c57e0108e02a30aa8780a61d
YashKumarVerma <yk.verma2000@gmail.com>
2016-04-25 13:53:28 +0530

    added sahayak dir

---
+++ sahayak.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* **************************************
+
+Sahayak
+=======
+The Command Line Tool for Blazing
+
+Load the module
+*****************************************/
+
+require_once "application/sahayak/load.php";
+
+
+print_r(exec("git add ."));
