404e0f8d2c5104fc996ca223ce3097ad53a2ab45
Yash Kumar Verma <yk.verma2000@gmail.com>
2016-09-24 00:34:11 +0530

    added checkPost
    no need to check separately for all elements by yourself. Simply pass the array containing post indexes you want to check. If all exist, then it would return 1, i.e. TRUE else not.

--- application/core/core.base.php
+++ application/core/core.base.php
@@ -37,6 +37,28 @@ class io
 
 	}
 	
+	static function postCheck($i)
+	{
+		// if it is an array
+		if(is_array($i))
+		{
+			// then loop through all elements 
+			$err = 0;
+			foreach ($i as $key) 
+			{ 
+				// if any unknown post index found, set error =1;
+				if(!isset($_POST[$key]))
+					$err=1;
+			}
+
+			// if there is error (error = 1)
+			if($err)
+				return 0; // 0 means postCheck Failed
+			else
+				return 1; // 1 means postCheck is successful
+		}
+	}
+
 	static function url()
 	{
 		return explode('/', $_GET['url'] );
