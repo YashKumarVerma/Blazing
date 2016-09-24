8bc9525d409a4c39b3072cdb64bd6246a885231f
Yash Kumar Verma <yk.verma2000@gmail.com>
2016-09-24 12:22:41 +0530

    Update core.base.php
    to check multiple isset over session

--- application/core/core.base.php
+++ application/core/core.base.php
@@ -37,6 +37,13 @@ class io
 
 	}
 	
+
+	static function url()
+	{
+		return explode('/', $_GET['url'] );
+	}
+
+// to check multiple isset conditions over post
 	static function postCheck($i)
 	{
 		// if it is an array
@@ -59,10 +66,28 @@ class io
 		}
 	}
 
-	static function url()
+// to check multiple isset over session
+	static function sessionCheck($i)
 	{
-		return explode('/', $_GET['url'] );
-	}
+		// if it is an array
+		if(is_array($i))
+		{
+			// then loop through all elements 
+			$err = 0;
+			foreach ($i as $key) 
+			{ 
+				// if any unknown post index found, set error =1;
+				if(!isset($_SESSION[$key]))
+					$err=1;
+			}
+
+			// if there is error (error = 1)
+			if($err)
+				return 0; // 0 means postCheck Failed
+			else
+				return 1; // 1 means postCheck is successful
+		}
+	}  
 
 }
 
