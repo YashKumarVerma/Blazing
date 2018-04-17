c52d133ea9abc3dd20d5e6ca2e51e56c03024653
Yash Kumar Verma <yk.verma2000@gmail.com>
2018-04-17 20:53:21 +0530

    CLI for route and controller insertion

--- sahayak.php
+++ sahayak.php
@@ -1,12 +1,55 @@
 <?php 
+/***************************************
+Sahayak 2.0
 
-/* **************************************
-
-Sahayak
-=======
 The Command Line Tool for Blazing
-
-Load the module
 *****************************************/
+$slug           = explode('/', $argv[2]);
+$controller     = $argv['1'];
+$method         = "";
+$parameterList  = [];
+$routeLine     = PHP_EOL."Route::"; 
+$controllerLine = PHP_EOL;
 
-require_once "application/sahayak/load.php";
+// if post request
+if($argc == 4){
+    $routeLine.= "post('";
+}else{
+    $routeLine.= "set('";
+}
+
+// get param list and method
+foreach($slug as $node){
+    if(strpos($node, '$') !== false){
+        array_push($parameterList,$node);
+    }
+    else{
+        $method.=ucwords($node);
+    }
+}
+
+/// line to add to routes
+$routeLine.=$argv[2].'\',\''.$controller.'@'.$method.'\');';
+
+
+// CONTROLLER DATA
+$controllerLine .= "function ".$method."(\$url){".PHP_EOL;
+foreach($parameterList as $node){
+    $controllerLine.= "\t".$node." = \$url['".$node."'];".PHP_EOL;
+}
+$controllerLine.= PHP_EOL."}";
+
+
+// Writing route
+$handle = file_get_contents("application/modules/routes.php");
+file_put_contents("application/modules/routes.php",$handle.$routeLine);
+
+// writing controller
+if(!file_exists("application/modules/controller/".$controller.".controller.php")){
+    $handle = fopen("application/modules/controller/".$controller.".controller.php","w");
+    fclose($handle);
+    $controllerLine = "<?php ".PHP_EOL.PHP_EOL.$controllerLine.PHP_EOL;
+}
+
+$handle = file_get_contents("application/modules/controller/".$controller.".controller.php");
+file_put_contents("application/modules/controller/".$controller.".controller.php",$handle.$controllerLine);
